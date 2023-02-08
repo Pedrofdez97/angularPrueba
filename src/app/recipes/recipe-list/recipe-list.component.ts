1f43db1a0ca4e9c29e8c7b3e9241dab1eade4b6c
Pedrofdez97 <pfernandezsalas@safareyes.es>
2023-02-08 20:12:37 +0100

    Prueba Angular

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test','https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_31/1895935/220804-recipe2x1-sj-119.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test','https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_31/1895935/220804-recipe2x1-sj-119.jpg' )
   ]
 
 
